37fe98be9855f78517747bf0e682bf6798684856
Carlos Rodas <crods57@gmail.com>
2023-04-04 13:50:11 -0500

    challenge 3, treasure island

---
+++ 100_days_of_coding/1_band-name-generator.py
@@ -0,0 +1,6 @@
+# Band name generator
+
+print("Welcome to the Band Name Generator")
+x = input("What's name of the city you grew up in?\n")
+y = input("What's your pet's name?\n")
+print(f"Your band name could be {x} {y}")
