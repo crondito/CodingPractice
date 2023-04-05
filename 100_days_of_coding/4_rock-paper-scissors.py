edfdead290b6270d518626b57909f54f2e5ecbe6
Carlos Rodas <crods57@gmail.com>
2023-04-05 08:44:08 -0500

    refactoring

--- 100_days_of_coding/4_rock-paper-scissors.py
+++ 100_days_of_coding/4_rock-paper-scissors.py
@@ -1,7 +1,7 @@
 # Rock, Paper, Scissors
 import random
 
-rock = '''
+ROCK = '''
 Rock
     _______
 ---'   ____)
@@ -11,7 +11,7 @@ Rock
 ---.__(___)
 '''
 
-paper = '''
+PAPER = '''
 Paper
     _______
 ---'   ____)____
@@ -21,7 +21,7 @@ Paper
 ---.__________)
 '''
 
-scissors = '''
+SCISSORS = '''
 Scissors
     _______
 ---'   ____)____
@@ -31,35 +31,36 @@ Scissors
 ---.__(___)
 '''
 
+WIN_TEXT = "You win!"
+
 player_choice = input(
     "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 computer_choice = random.randint(0, 2)
 
 if player_choice == "0":
-    print(rock)
+    print(ROCK)
 elif player_choice == "1":
-    print(paper)
+    print(PAPER)
 elif player_choice == "2":
-    print(scissors)
+    print(SCISSORS)
 else:
     print("Invalid input. You lose!\n")
 
 print("Computer chose:")
 if computer_choice == 0:
-    print(rock)
+    print(ROCK)
 elif computer_choice == 1:
-    print(paper)
+    print(PAPER)
 elif computer_choice == 2:
-    print(scissors)
+    print(SCISSORS)
 
 if int(player_choice) == computer_choice:
     print("It's a draw.")
 elif player_choice == "1" and computer_choice == 0:
-    print("You win!")
-
+    print(WIN_TEXT)
 elif player_choice == "2" and computer_choice == 1:
-    print("You win!")
+    print(WIN_TEXT)
 elif player_choice == "0" and computer_choice == 2:
-    print("You win!")
+    print(WIN_TEXT)
 else:
     print("You lose!")
