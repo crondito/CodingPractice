0c51c0732fe6b03ba626ce2f397dc5d236081468
Carlos Rodas <crods57@gmail.com>
2023-06-27 10:50:13 -0500

    setting up tests

--- LeetCode/Arrays & Hashing/3_two_sum.py
+++ LeetCode/Arrays & Hashing/3_two_sum.py
@@ -3,3 +3,22 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 #
 # You can return the answer in any order.
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return [1]  # add logic here
+
+
+solution = Solution()
+
+result1 = solution.twoSum([2, 7, 11, 15], 9)
+print(result1)  # [0,1]
+
+result2 = solution.twoSum([3, 2, 4], 6)
+print(result2)  # [1,2]
+
+result3 = solution.twoSum([3, 3], 6)
+print(result3)  # [0,1]
