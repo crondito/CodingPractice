6f01373b1f6cbafbe0d2cdac96a6febcd721b5a7
Carlos Rodas <crods57@gmail.com>
2023-07-02 13:18:20 -0500

    product except optimized

--- LeetCode/Arrays & Hashing/6_product_except_self.py
+++ LeetCode/Arrays & Hashing/6_product_except_self.py
@@ -12,9 +12,20 @@ import math
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = []
-        for i in range(len(nums)):
-            result.append(math.prod(nums[:i] + nums[i + 1 :]))
+        n = len(nums)
+        left_products = [1] * n
+        right_products = [1] * n
+        result = [1] * n
+
+        for i in range(1, n):
+            left_products[i] = left_products[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            right_products[i] = right_products[i + 1] * nums[i + 1]
+
+        for i in range(n):
+            result[i] = left_products[i] * right_products[i]
+
         return result
 
 
