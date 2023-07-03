4543e3698b3103c9c508895266f28c74cf61ea9c
Carlos Rodas <crods57@gmail.com>
2023-07-03 12:17:48 -0500

    valid sudoku complete

--- LeetCode/Arrays & Hashing/7_valid_sudoku.py
+++ LeetCode/Arrays & Hashing/7_valid_sudoku.py
@@ -10,11 +10,39 @@
 # Only the filled cells need to be validated according to the mentioned rules.
 
 from typing import List
+import numpy as np
 
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        return False  # TO DO
+        arr = np.array(board)
+        transposed_arr = arr.transpose()
+
+        if not self.check_no_repeated_numbers(arr):
+            return False
+
+        if not self.check_no_repeated_numbers(transposed_arr):
+            return False
+
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                aux_arr = arr[i : i + 3, j : j + 3]
+                values = set()
+                for element in aux_arr.flatten():
+                    if element.isdigit() and element in values:
+                        return False
+                    values.add(element)
+
+        return True
+
+    def check_no_repeated_numbers(self, arr: np.array) -> bool:
+        for column in arr:
+            filtered_column = column[np.char.isnumeric(column)]
+            numeric_column = filtered_column.astype(int)
+            if len(np.unique(numeric_column)) != len(numeric_column):
+                return False
+
+        return True
 
 
 solution = Solution()
@@ -48,3 +76,18 @@ result2 = solution.isValidSudoku(
     ]
 )
 print(result2)  # False
+
+result3 = solution.isValidSudoku(
+    [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", "8", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", "2", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+    ]
+)
+print(result3)  # False
