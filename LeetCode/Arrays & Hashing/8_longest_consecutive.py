f34bdb743af87848ba4720830d346f78b2df047e
Carlos Rodas <crods57@gmail.com>
2023-07-04 08:24:28 -0500

    longest consecutive completed

--- LeetCode/Arrays & Hashing/8_longest_consecutive.py
+++ LeetCode/Arrays & Hashing/8_longest_consecutive.py
@@ -7,7 +7,22 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        return 0  # TO DO
+        num_set = set(nums)
+        print(num_set)
+        longest_streak = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
 
 
 solution = Solution()
