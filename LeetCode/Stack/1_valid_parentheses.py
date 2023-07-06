367ebf887a46bca01237407f709512c6d7ef25e2
Carlos Rodas <crods57@gmail.com>
2023-07-06 13:11:44 -0500

    valid parentheses complete

--- LeetCode/Stack/1_valid_parentheses.py
+++ LeetCode/Stack/1_valid_parentheses.py
@@ -6,9 +6,30 @@
 # Open brackets must be closed in the correct order.
 # Every close bracket has a corresponding open bracket of the same type.
 
+from typing import List
+
 class Solution:
+    def check_parentheses(self, lst: List[str]):
+        stack = []
+        opening_chars = "({["
+        closing_chars = ")}]"
+        matching_chars = {")": "(", "]": "[", "}": "{"}
+
+        for element in lst:
+            if element in opening_chars:
+                stack.append(element)
+            elif element in closing_chars:
+                if len(stack) == 0 or stack[-1] != matching_chars[element]:
+                    return False
+                else:
+                    stack.pop()
+
+        return len(stack) == 0
+
     def isValid(self, s: str) -> bool:
-        return False # TO DO
+        return self.check_parentheses(list(s))
+    
+    
     
 solution = Solution()
 
@@ -20,3 +41,6 @@ print(result2)  # True
 
 result3 = solution.isValid("(]")
 print(result3)  # False
+
+result4 = solution.isValid(")(")
+print(result4)  # False
